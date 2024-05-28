e641ae731f88ae064905f0ef1d2befbf91d270b3
dzgierski19 <dzgierskigit@gmail.com>
2024-05-29 01:23:16 +0200

    integration testing

--- src/test/HttpClient.test.ts
+++ src/test/HttpClient.test.ts
@@ -1,13 +1,28 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import axios from "axios";
+axios.defaults.adapter = "http";
 import nock from "nock";
 import request from "supertest";
 import { app } from "../main";
 import { Server } from "http";
-import axios from "axios";
-axios.defaults.adapter = "http";
 
 describe("WeatherService test suite", () => {
+  const key = process.env.API_KEY;
+
+  const queryForCurrentReq = {
+    unitGroup: "metric",
+    key: key,
+    contentType: "json",
+    include: "current",
+  };
+
+  const queryForPastOrFutureReq = {
+    unitGroup: "metric",
+    key: key,
+    contentType: "json",
+  };
+
   describe("API Integration Tests", () => {
     let server: Server;
     beforeAll(() => {
@@ -22,51 +37,75 @@ describe("WeatherService test suite", () => {
     afterAll(() => {
       server.close();
     });
-    test("testing city endpoint", async () => {
+    test("testing current weather in given city endpoint", async () => {
       const city = "Lublin";
-      const key = process.env.API_KEY;
-      const date = new Date().toISOString();
-      const scope = nock(
-        //   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
-        // )
-        "http://localhost:2000"
-      )
-        .get(`/weather/cities/${city}`)
-        .reply(200, { value: 12 });
-      //   // .query({
-      //   //   unitGroup: "metric",
-      //   //   key: "THMT9Q32G8HSPQZDPZA4QZEJR",
-      //   //   contentType: "json",
-      //   //   include: "current",
-      //   // })
-      //   .reply(200, { value: "12" });
-      const response = await request(server).get(`/weather/cities/${city}`);
-      console.log("Request made to server");
-      console.log("Response body:", response.body);
+      const scope = nock(process.env.EXTERNAL_API)
+        .get(`/${city}`)
+        .query(queryForCurrentReq)
+        .reply(200, { currentConditions: { temp: 12 } });
 
-      // Check if the Nock interceptor was used
-      console.log("Is Nock interceptor done?", scope.isDone());
+      const response = await request(server).get(`/weather/cities/${city}`);
+
+      expect(scope.isDone()).toBe(true);
+      expect(response.body).toBe(12);
+      expect(response.statusCode).toBe(200);
+    });
+    test("testing current weather in given location endpoint", async () => {
+      const lat = 51.247;
+      const lon = 22.566;
+      const space = "%2C%20";
+      const scope = nock(process.env.EXTERNAL_API)
+        .get(`/${lat + space + lon}`)
+        .query(queryForCurrentReq)
+        .reply(200, { currentConditions: { temp: 20 } });
+
+      const response = await request(server).get(
+        `/weather/locations/${lat}/${lon}`
+      );
+
+      expect(scope.isDone()).toBe(true);
+      expect(response.body).toBe(20);
+      expect(response.statusCode).toBe(200);
+    });
+    test("testing historical weather in date range for city endpoint", async () => {
+      const city = "Warsaw";
+      const dateFrom = "2023-11-10";
+      const dateTo = "2023-11-23";
+      const scope = nock(process.env.EXTERNAL_API)
+        .get(`/${city}/${dateFrom}/${dateTo}`)
+        .query(queryForPastOrFutureReq)
+        .reply(200, {
+          days: [{ datetime: "2023-11-22", temp: 10 }],
+        });
+      const response = await request(server)
+        .get(`/weather/historical/cities/${city}`)
+        .query({ dateFrom: dateFrom, dateTo: dateTo });
+
+      console.log(response.body);
+      // expect(scope.isDone()).toBe(true);
+      expect(response.body).toEqual([["2023-11-22", 10]]);
+      expect(response.statusCode).toBe(200);
+    });
+    test("testing predicted weather in date range in given location endpoint", async () => {
+      const lat = 51.247;
+      const lon = 22.566;
+      const space = "%2C%20";
+      const dateFrom = "2023-11-10";
+      const dateTo = "2023-11-23";
+      const scope = nock(process.env.EXTERNAL_API)
+        .get(`/${lat + space + lon}/${dateFrom}/${dateTo}`)
+        .query(queryForPastOrFutureReq)
+        .reply(200, {
+          days: [{ datetime: "2024-02-20", temp: 100 }],
+        });
+      const response = await request(server)
+        .get(`/weather/historical/locations/${lat}/${lon}`)
+        .query({ dateFrom: dateFrom, dateTo: dateTo });
+
       console.log(response.body);
       expect(scope.isDone()).toBe(true);
-      expect(response.body).toBe(24.8);
+      expect(response.body).toEqual([["2024-02-20", 100]]);
       expect(response.statusCode).toBe(200);
     });
   });
 });
-// supertest
-// polly
-// describe("API Integration Tests", () => {
-//   let server: Server;
-//   beforeEach(() => {
-//     server = app.listen(2000);
-//   });
-//   afterEach(() => {
-//     server.close();
-//   });
-//   it("should return status 200", async () => {
-//     const response = await request(server).get("/weather/cities/Lublin");
-//     expect(response.status).toBe(200);
-//   });
-// });
-
-// request => validacje => controlera => serwisu => adaptera => (zamokowane przez ciebie dane) i jest sprawdznie, ze musi byc  => serwisu => controlera => klient
