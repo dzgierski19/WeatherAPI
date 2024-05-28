c5e0400e671dcb382ff787f5633fb40fce0951cc
dzgierski19 <dzgierskigit@gmail.com>
2024-05-28 16:55:11 +0200

    added validatorMiddleware in routes and cleaned code

--- src/domains/weather/WeatherRouter.ts
+++ src/domains/weather/WeatherRouter.ts
@@ -1,36 +1,55 @@
 import { Router } from "express";
 import { weatherController } from "./../../IoC";
+import { validatorMiddleware } from "../middlewares/ValidationMiddleware";
+import {
+  getCurrentWeatherForCitySchema,
+  getCurrentWeatherForLocationSchema,
+  getWeatherForCityInDateRangeSchema,
+  getWeatherForLocationInDateRangeSchema,
+} from "../../schemas/ValidateSchemas";
 
 require("express-async-errors");
 
 export const weatherRouter = Router();
 
+// CURRENT WEATHER
+
 weatherRouter.get(
   "/weather/cities/:city",
+  validatorMiddleware(getCurrentWeatherForCitySchema),
   weatherController.getCurrentWeatherForCity
 );
 
 weatherRouter.get(
   "/weather/locations/:lat/:lon",
+  validatorMiddleware(getCurrentWeatherForLocationSchema),
   weatherController.getCurrentWeatherForLocation
 );
 
+// HISTORICAL WEATHER
+
 weatherRouter.get(
-  "/weather/historical/:date/:city/",
-  weatherController.getHistoricalWeatherForCity
+  "/weather/historical/cities/:city",
+  validatorMiddleware(getWeatherForCityInDateRangeSchema),
+  weatherController.getHistoricalWeatherForCityInDateRange
 );
 
 weatherRouter.get(
+  "/weather/historical/location/:lat/:lon",
+  validatorMiddleware(getWeatherForLocationInDateRangeSchema),
+  weatherController.getHistoricalWeatherForLocationInDateRange
+);
+
+// PREDICTED WEATHER
+
+weatherRouter.get(
   "/weather/prediction/cities/:city",
+  validatorMiddleware(getWeatherForCityInDateRangeSchema),
   weatherController.getWeatherPredictionForCityInDateRange
 );
 
 weatherRouter.get(
   "/weather/prediction/location/:lat/:lon",
+  validatorMiddleware(getWeatherForLocationInDateRangeSchema),
   weatherController.getWeatherPredictionForLocationInDateRange
-);
-
-weatherRouter.get(
-  "/weather/prediction/:date/:city",
-  weatherController.getWeatherPredictionForCity
 );
