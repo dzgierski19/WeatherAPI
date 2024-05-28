98343f8a98dcd939015aafcd516e01240e9c0f7e
dzgierski19 <dzgierskigit@gmail.com>
2024-05-28 16:47:45 +0200

    changed app conception to create AxiosClient firstly

--- src/IoC.ts
+++ src/IoC.ts
@@ -1,5 +1,9 @@
 import { WeatherController } from "./domains/weather/WeatherController";
 import { WeatherService } from "./domains/weather/WeatherService";
+import { AxiosClient } from "./domains/AxiosClient/AxiosClient";
+import { WeatherHttpClient } from "./domains/HttpClient/WeatherHttpClient";
 
-export const weatherService = new WeatherService();
+export const axiosClient = new AxiosClient();
+export const weatherHttpClient = new WeatherHttpClient(axiosClient);
+export const weatherService = new WeatherService(weatherHttpClient);
 export const weatherController = new WeatherController(weatherService);
