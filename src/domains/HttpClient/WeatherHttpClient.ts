4eccb36b40110a5dcbbc5a301be17ffb2ebac314
dzgierski19 <dzgierskigit@gmail.com>
2024-05-28 16:52:04 +0200

    weatherHttpClient to handle responses for past, future and current dates

---
+++ src/domains/HttpClient/WeatherHttpClient.ts
@@ -0,0 +1,46 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { AxiosError, AxiosResponse } from "axios";
+import { unitGroup } from "./HttpClientTypes";
+import { AxiosClient } from "../AxiosClient/AxiosClient";
+
+// axios instance z odpowiednimi danymi
+
+// WeatherHttpClient
+
+// zodowa schema jak chce zeby wygladal config
+
+export interface IWeatherHttpClient {
+  getCurrentData(url: string): Promise<AxiosResponse>;
+  getPastOrFutureData(url: string): Promise<AxiosResponse>;
+}
+
+export class WeatherHttpClient implements IWeatherHttpClient {
+  constructor(private readonly axiosClient: AxiosClient) {}
+
+  async getCurrentData(url: string) {
+    const createdAxios = this.createAxiosInstance();
+    try {
+      const response = await createdAxios.get(url, {
+        params: { include: "current" },
+      });
+      return response;
+    } catch (err) {
+      throw new AxiosError("Axios Error");
+    }
+  }
+
+  async getPastOrFutureData(url: string) {
+    const axiosClient = this.createAxiosInstance();
+    try {
+      const response = await axiosClient.get(url);
+      return response;
+    } catch (err) {
+      throw new AxiosError("Axios Error");
+    }
+  }
+
+  private createAxiosInstance() {
+    return this.axiosClient.createAxiosInstance();
+  }
+}
