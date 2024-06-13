c488ec1a651e867e14d466ff7aff1ef0cf14391f
dzgierski19 <dzgierskigit@gmail.com>
2024-06-13 08:21:58 +0200

    changed file location

--- src/domains/AxiosClient/ConfigSchema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const configSchema = z.object({
-  baseURL: z.string().url().optional(),
-  apiKey: z.string().optional(),
-  unitGroup: z.enum(["metric", "uk", "us", "base"]).optional(),
-  contentType: z.enum(["json", "csv"]).optional(),
-});
