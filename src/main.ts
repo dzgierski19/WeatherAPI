32445bed693b4c4709fb8b8c4475e1849fcbc99c
dzgierski19 <dzgierskigit@gmail.com>
2024-06-13 08:25:51 +0200

    created function to run app

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { weatherRouter } from "./domains/weather/WeatherRouter";
-import { errorHandler } from "./domains/middlewares/ErrorHandler";
+import { errorHandler } from "./app/middlewares/ErrorHandler";
 
-export const app = express();
+const bootstrap = () => {
+  const app = express();
+  app.get("/", (req, res) => {
+    console.log("it's working");
+    res.send("response from server");
+  });
 
-app.get("/", (req, res) => {
-  console.log("it's working");
-  res.send("response from server");
-});
+  app.use(weatherRouter);
+  app.use(errorHandler);
+  return app;
+};
 
-app.use(weatherRouter);
-app.use(errorHandler);
+export const app = bootstrap();
