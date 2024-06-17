571562a5db0a02ede75eeb3c82b674c43a74b9ca
dzgierski19 <dzgierskigit@gmail.com>
2024-06-17 19:30:36 +0200

    included config in zod validator

--- src/domains/weather/app/AxiosClient/ConfigSchema.ts
+++ src/domains/weather/app/AxiosClient/ConfigSchema.ts
@@ -1,4 +1,5 @@
 import z, { ZodSchema } from "zod";
+import { configDataType } from "./ConfigTypes";
 
 export class Validator {
   static run<T>(schema: ZodSchema<T>, data: Record<string, unknown>): T {
@@ -6,7 +7,9 @@ export class Validator {
   }
 }
 
-export const weatherApiConfig = Validator.run(
-  z.object({ baseUrl: z.string().url(), apiKey: z.string() }),
-  { baseUrl: process.env.EXTERNAL_API, apiKey: process.env.API_KEY }
-);
+export const weatherApiConfig = (config: configDataType) => {
+  Validator.run(z.object({ baseUrl: z.string().url(), apiKey: z.string() }), {
+    baseUrl: config.baseUrl,
+    apiKey: config.apiKey,
+  });
+};
