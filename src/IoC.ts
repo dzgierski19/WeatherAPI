efe2bc3990b9029049f812fccab2657fb815ed87
dzgierski19 <dzgierskigit@gmail.com>
2024-06-17 19:30:48 +0200

    create config for app instance

--- src/IoC.ts
+++ src/IoC.ts
@@ -2,8 +2,13 @@ import { WeatherController } from "./domains/weather/WeatherController";
 import { WeatherService } from "./domains/weather/WeatherService";
 import { WeatherHttpAdapter } from "./domains/weather/adapters/HttpClient/WeatherHttpAdapter";
 import { WeatherApiClient } from "./domains/weather/app/AxiosClient/AxiosClient";
+import { configDataType } from "./domains/weather/app/AxiosClient/ConfigTypes";
 
-export const axiosClient = new WeatherApiClient();
+const config: configDataType = {
+  baseUrl: process.env.EXTERNAL_API,
+  apiKey: process.env.API_KEY,
+};
+export const axiosClient = new WeatherApiClient(config);
 export const weatherHttpAdapter = new WeatherHttpAdapter(axiosClient);
 export const weatherService = new WeatherService(weatherHttpAdapter);
 export const weatherController = new WeatherController(weatherService);
