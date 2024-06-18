9cfb2d09a027adac47e84b03201a133420943c0b
dzgierski19 <dzgierskigit@gmail.com>
2024-06-18 09:43:31 +0200

    added validator in constructor

--- src/domains/weather/app/AxiosClient/AxiosClient.ts
+++ src/domains/weather/app/AxiosClient/AxiosClient.ts
@@ -1,18 +1,16 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import axios, { AxiosInstance } from "axios";
-import { weatherApiConfig } from "./ConfigSchema";
-import { configDataType } from "./ConfigTypes";
+import { configDataType, weatherApiConfig } from "./ConfigSchema";
 
 export class WeatherApiClient {
   api: AxiosInstance;
 
-  constructor(private readonly config: configDataType) {
+  constructor(private readonly config: configDataType = weatherApiConfig) {
     this.api = this.createAxiosInstance();
   }
 
   private createAxiosInstance(): AxiosInstance {
-    weatherApiConfig(this.config);
     return axios.create({
       baseURL: this.config.baseUrl,
       params: {
