76e65c94f01200e92e091b725450585a6b37d5c0
dzgierski19 <dzgierskigit@gmail.com>
2024-06-18 09:42:08 +0200

    deleted config

--- src/IoC.ts
+++ src/IoC.ts
@@ -2,13 +2,9 @@ import { WeatherController } from "./domains/weather/WeatherController";
 import { WeatherService } from "./domains/weather/WeatherService";
 import { WeatherHttpAdapter } from "./domains/weather/adapters/HttpClient/WeatherHttpAdapter";
 import { WeatherApiClient } from "./domains/weather/app/AxiosClient/AxiosClient";
-import { configDataType } from "./domains/weather/app/AxiosClient/ConfigTypes";
+import { configDataType } from "./domains/weather/app/AxiosClient/ConfigSchema";
 
-const config: configDataType = {
-  baseUrl: process.env.EXTERNAL_API,
-  apiKey: process.env.API_KEY,
-};
-export const axiosClient = new WeatherApiClient(config);
+export const axiosClient = new WeatherApiClient();
 export const weatherHttpAdapter = new WeatherHttpAdapter(axiosClient);
 export const weatherService = new WeatherService(weatherHttpAdapter);
 export const weatherController = new WeatherController(weatherService);
